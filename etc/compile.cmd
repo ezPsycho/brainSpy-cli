d7fab33cecf7a0bc68719ecf81f90699d984952f
Losses Don <losses.don@gmail.com>
2019-01-12 22:15:35 +0800

    New compiling script.

--- etc/compile.cmd
+++ etc/compile.cmd
@@ -1,14 +1,8 @@
 cd ..
-rmdir "dist/brainSpy" /S /Q
-pyinstaller ./brainSpy.py --icon=etc/assets/brainSpy_win32.ico
-cd modules/brainSpy
-cd ..
-rmdir "__pycache__" /S /Q
-cd ..
-md "dist/brainSpy/modules/brainspy"
-xcopy "modules/brainspy" "dist/brainSpy/modules/brainspy" /Y /S
-cd "dist/brainSpy"
-del "VCRUNTIME140.dll"
-cd "modules/brainSpy/data"
-del "*.zip"
-cd ../../../../
+rmdir "dist\brainSpy" /S /Q 
+pyinstaller ./brainSpy.spec
+mkdir "dist\brainSpy\data"
+xcopy ".\src\data" ".\dist\brainSpy\data" /S /Y
+cd "dist\brainSpy"
+del "VCRUNTIME140.dll" /Q
+cd ../..
