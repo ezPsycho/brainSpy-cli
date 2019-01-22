23e1f8e4cb0eb49fb813c95706b1a096f9adfe8a
Losses Don <losses.don@gmail.com>
2019-01-22 18:47:54 +0800

    Drop `tk`.

--- src/readers/clipboard.py
+++ src/readers/clipboard.py
@@ -1,25 +1,18 @@
 import re
 import sys
 import time
-from tkinter import Tk
+import pyperclip
 
 class Reader:
     def __init__(self, args):
         self.loop = False
     
     def run(self, parser):
-        # Read content from clipboard with Tk:
-        root = Tk()
-        root.withdraw()
-        time.sleep(0.1)
-
         try:
-            clipboard_content = root.clipboard_get()
+            clipboard_content = pyperclip.paste()
         except:
             print('[ERROR] failed to get content from the clipboard.')
             sys.exit(1)
-        
-        root.destroy()
 
         clipboard_rows = re.split(r'[~\r\n]+', clipboard_content)
 
